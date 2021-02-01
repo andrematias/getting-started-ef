f8f609698b85bbc45e64638141ff4c77fb80f820
André Matias <dev.andrematias@gmail.com>
2021-02-01 12:40:24 -0300

    Create a console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFProductControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
