e4615140065a8d773a2929f363625453accb0c55
André Matias <dev.andrematias@gmail.com>
2021-02-02 17:34:13 -0300

    Add Produtos and Clientes contexts

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -6,6 +6,8 @@ namespace EFProductControl.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
