1ee3ba091b45e8a61e68412037cfe123e3740b73
André Matias <dev.andrematias@gmail.com>
2021-02-04 22:46:54 -0300

    Include multiple entities into database
    To include multiple entities we can use methods from dbcontext as when
    we insert single entities, but we use AddRange method.
    
    Including by multiple parammeters:
    db.AddRange(entityOne, entityTwo);
    
    Or we can use a list of entities:
    db.AddRange(List<Entity>);

--- Program.cs
+++ Program.cs
@@ -8,7 +8,36 @@ namespace EFProductControl
     {
         static void Main(string[] args)
         {
-            InserirDados();
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            using var db = new Data.ApplicationContext();
+            var clientes = new[] {
+                new Cliente
+                {
+                    Nome = "José",
+                    CEP = "06355620",
+                    Cidade = "Carapicuiba",
+                    Estado = "SP",
+                    Telefone = "11 985019631"
+                },
+                new Cliente
+                {
+                    Nome = "Lucia",
+                    CEP = "06355620",
+                    Cidade = "Carapicuiba",
+                    Estado = "SP",
+                    Telefone = "11 985019631"
+                }
+            };
+
+            db.Clientes.AddRange(clientes);
+            //db.Set<Cliente>().AddRange(clientes);
+            //db.AddRange(clientes);
+            var total = db.SaveChanges();
+            Console.WriteLine($"Total de inclusões: {total}");
         }
 
         private static void InserirDados()
