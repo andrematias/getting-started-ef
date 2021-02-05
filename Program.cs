bfa2072818ce8a6d86c8f00c5b5d8c2b3598516f
André Matias <dev.andrematias@gmail.com>
2021-02-04 23:22:13 -0300

    Include single entity into database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using EFProductControl.Domain;
+using EFProductControl.ValueObjects;
 
 namespace EFProductControl
 {
@@ -6,7 +8,30 @@ namespace EFProductControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoDeBarras = "123123123",
+                Valor = "$10,00",
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = false
+            };
+
+            using( var db= new Data.ApplicationContext() )
+            {
+                //We can add data as following
+                db.Produtos.Add(produto);
+                //db.Set<Produto>().Add(produto);
+                //db.Add(produto);
+
+                var registros = db.SaveChanges();
+                Console.WriteLine($"Total de registros salvos: {registros}");
+            }
         }
     }
 }
